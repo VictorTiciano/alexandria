65b2496ec58242617a2b590f2e52118b4053caa7
Victor Vasconcelos <victorvasconcelos2017@gmail.com>
2022-01-26 00:20:29 -0300

    correção de arquivo

--- backend/src/main/java/com/ufc/quixada/alexandria/services/ClienteService.java
+++ backend/src/main/java/com/ufc/quixada/alexandria/services/ClienteService.java
@@ -3,6 +3,7 @@ package com.ufc.quixada.alexandria.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -52,10 +53,9 @@ public class ClienteService {
 	
 	@Transactional
 	public ClienteDTO atualizar(ClienteDTO dto, Long id) {
-		
-        Cliente cliente;
 
-        cliente = repository.findById(id).get();       
+        Cliente cliente;
+		cliente = repository.findById(id).get();       
     	cliente.setId(id);
     	cliente.setNome(dto.getNome());
     	cliente.setEndereco(dto.getEndereco());
