225f114ab102119db94c313ac143de912e337603
Fernando Sousa <luis.nando.acarau@gmail.com>
2022-01-26 00:43:34 -0300

    adm tratamento de erros

--- backend/src/main/java/com/ufc/quixada/alexandria/controllers/AdministradorController.java
+++ backend/src/main/java/com/ufc/quixada/alexandria/controllers/AdministradorController.java
@@ -3,6 +3,7 @@ package com.ufc.quixada.alexandria.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ufc.quixada.alexandria.dto.AdministradorDTO;
+import com.ufc.quixada.alexandria.dto.ClienteDTO;
+import com.ufc.quixada.alexandria.repositories.AdministradorRepository;
+import com.ufc.quixada.alexandria.repositories.ClienteRepository;
 import com.ufc.quixada.alexandria.services.AdministradorService;
 
 @RestController
@@ -22,31 +26,60 @@ public class AdministradorController {
 	@Autowired
 	private AdministradorService service;
 	
+	@Autowired
+	private AdministradorRepository repository;
+	
 	@GetMapping
 	public Page<AdministradorDTO> findAll(Pageable pageable){
 		return service.findAll(pageable);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public AdministradorDTO findById(@PathVariable Long id){
-		return service.findById(id);
+	public ResponseEntity<Object> findById(@PathVariable Long id){
+		boolean existsAdm = repository.existsById(id);
+		
+		if(existsAdm) {
+			AdministradorDTO adm = new AdministradorDTO(repository.findById(id).get());
+			service.findById(id);
+			return ResponseEntity.ok(adm);
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@PostMapping(value = "/add")
-    public AdministradorDTO saveAdministrador(@RequestBody AdministradorDTO dto) {
-		AdministradorDTO administradorDTO = service.salvar(dto);
-		return administradorDTO;
+    public ResponseEntity<Object> saveAdministrador(@RequestBody AdministradorDTO dto) {
+		boolean existsCpf = repository.existsByCpf(dto.getCpf());
+		boolean existsEmail = repository.existsByEmail(dto.getEmail());
+		if(existsCpf) {
+			return ResponseEntity.badRequest().body("CPF já cadastrado");
+		}
+		if(existsEmail) {
+			return ResponseEntity.badRequest().body("Email já cadastrado");
+		}
+		service.salvar(dto);
+		return ResponseEntity.ok("Administrador registrado com sucesso");
     }
 	
 	@DeleteMapping(value = "/{id}")
-	public void deletarCliente(@PathVariable Long id) {
-		service.deletar(id);
+	public ResponseEntity<Object> deletarAdministrador(@PathVariable Long id) {
+		boolean existsAdm = repository.existsById(id);
+		if(existsAdm) {
+			service.deletar(id);
+			return ResponseEntity.ok("Administrador deletado com sucesso");
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@PutMapping(value = "/{id}")
-	public AdministradorDTO atualizarCliente( @RequestBody AdministradorDTO dto, @PathVariable Long id) {
-		AdministradorDTO administradorDTO = service.atualizar(dto, id);
-		return administradorDTO;
+	public ResponseEntity<Object> atualizar( @RequestBody AdministradorDTO dto, @PathVariable Long id) {
+		boolean existsAdm = repository.existsById(id);
+		
+		if(existsAdm) {
+			service.atualizar(dto, id);
+			return ResponseEntity.ok("Administrador atualizado com sucesso");
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
--- backend/src/main/java/com/ufc/quixada/alexandria/repositories/AdministradorRepository.java
+++ backend/src/main/java/com/ufc/quixada/alexandria/repositories/AdministradorRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ufc.quixada.alexandria.entities.Administrador;
 
 public interface AdministradorRepository extends JpaRepository< Administrador, Long>{
-
+	boolean existsByCpf(String cpf);
+	
+	boolean existsByEmail(String email);
 }
